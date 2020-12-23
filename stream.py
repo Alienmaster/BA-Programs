225eef4dfbc28903ac512cba8a00f20384df5e34
Robert Geislinger <mitachundkrach@gmail.com>
2020-12-23 14:50:33 +0100

    Update stream.py
    Update 22.12

--- stream.py
+++ stream.py
@@ -5,7 +5,7 @@ import json
 
 
 #Set the filename and open the file
-filename = '/var/freeswitch/meetings/file.wav'
+filename = '/var/freeswitch/meetings/2020-12-22-19-52-01_ECHO_TO_CONFERENCE_write.wav'
 file = open(filename,'rb', buffering=2048)
 
 #Find the size of the file and move to the end
@@ -17,21 +17,22 @@ red = redis.Redis(host="localhost")
 
 pubsub = red.pubsub()
 
-pubsub.subscribe("audio_read", "from-akka-apps-redis-channel")
+pubsub.subscribe("audio_read")
 
 meeting = 0
 while True:
     text = pubsub.get_message()
+    # if text:
+    #     if text["data"] not in {1, 2}:
+    #         text = json.loads(text["data"].decode("UTF-8"))
+    #         print(text["core"]["header"]["name"])
+    #         if text["core"]["header"]["name"] == "MeetingCreatedEvtMsg":
+    #             meeting = text["core"]["body"]["props"]["voiceProp"]["voiceConf"]
+    #             print(meeting)
+    #         if text["core"]["header"]["name"] == "MeetingDestroyedEvtMsg":
+    #             os.system("rm -f /var/freeswitch/meetings/%s.wav" % meeting)
     if text:
-        if text["data"] not in {1, 2}:
-            text = json.loads(text["data"].decode("UTF-8"))
-            print(text["core"]["header"]["name"])
-            if text["core"]["header"]["name"] == "MeetingCreatedEvtMsg":
-                meeting = text["core"]["body"]["props"]["voiceProp"]["voiceConf"]
-                print(meeting)
-            if text["core"]["header"]["name"] == "MeetingDestroyedEvtMsg":
-                os.system("rm -f /var/freeswitch/meetings/%s.wav" % meeting)
-
+        print(text)
     where = file.tell()
     line = file.read()
     if not line:
