f3cdc6e30fc94a6fc68871d83070b54e87b8d444
bbb <bbb@ltbbb1.informatik.uni-hamburg.de>
2021-01-02 22:30:38 +0100

    first kaldi starter

--- kaldi_starter.py
+++ kaldi_starter.py
@@ -1,12 +1,55 @@
 import os
 import redis
+import json
+import multiprocessing as mp
 
 red = redis.Redis(host="ltbbb2", port=6379, password="")
 
-def start_kaldi():
-    os.system("../projects/")
+data_channel = "test_channel"
+
+kaldi_instances = {}
+
+def start_kaldi(input, output):
+
+    os.system("/home/bbb/ba/kaldi_modelserver_bbb/pykaldi_bbb_env/bin/python3.7 /home/bbb/ba/kaldi_modelserver_bbb/nnet3_model.py -m 0 -e -t -y /home/bbb/ba/kaldi_modelserver_bbb/models/kaldi_tuda_de_nnet3_chain2.yaml --redis-audio=%s --redis-channel=%s -fpc 190" %  (input, output))
+
+
+def wait_for_channel():
+    pubsub = red.pubsub()
+    pubsub.subscribe(data_channel)
+
+    while True:
+        message = pubsub.get_message()
+        if message and message["data"] != 1:
+            message = json.loads(message["data"].decode("UTF-8"))
+            print(message)
+            try:
+                input_channel = message["ASR-Channel"]
+                output_channel = message["Caller-Orig-Caller-ID-Name"].replace(" ", ".") + "_data"
+                CallerDestinationNumber = message["Caller-Destination-Number"]
+                OrigCallerIDName = message["Caller-Orig-Caller-ID-Name"]
+                
+                if message["Event"] == "LOADER_START":
+                    print("Start Kaldi")
+                    p = mp.Process(target=start_kaldi, args=(input_channel, output_channel,))
+                    p.start()
+                    kaldi_instances[input_channel] = p
+                    
+                    Loader_Start_msg = {"Event" : "KALDI_START", "Caller-Destination-Number" : CallerDestinationNumber, "Caller-Orig-Caller-ID-Name" : OrigCallerIDName, "Input-Channel" : input_channel, "ASR-Channel" : output_channel}
+                    red.publish(data_channel, json.dumps(Loader_Start_msg))
+                
+                if message["Event"] == "LOADER_STOP":
+                    input_channel = message["ASR-Channel"]
+                    print("Stop Kaldi")
+                    p = kaldi_instances.pop(input_channel, None)
+                    if p:
+                        p.terminate()
+                        Loader_Stop_msg = {"Event" : "KALDI_START", "Caller-Destination-Number" : CallerDestinationNumber, "Caller-Orig-Caller-ID-Name" : OrigCallerIDName, "Input-Channel" : input_channel, "ASR-Channel" : output_channel}
+                        red.publish(data_channel, json.dumps(Loader_Stop_msg))
+            except:
+                pass
 
 
 
 if __name__ == "__main__":
-    start_kaldi()
+    wait_for_channel()
