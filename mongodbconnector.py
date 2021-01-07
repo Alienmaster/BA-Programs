4e5207b8ecaf900c26ddb1e1679bc33bf24f47e8
Alienmaster <mitachundkrach@gmail.com>
2021-01-07 21:48:59 +0100

    bring data together :)

--- mongodbconnector.py
+++ mongodbconnector.py
@@ -6,13 +6,17 @@ from multiprocessing import Manager
 
 myclient = pymongo.MongoClient("mongodb://127.0.1.1:27017")
 
+### REDIS ###
 red = redis.Redis(host="localhost", port=6379, password="")
+pubsub = red.pubsub()
+pubsub.subscribe("test_channel")
 
 mydb = myclient["meteor"]["captions"]
 
-manager = Manager()
-u = manager.list()
-meeting = manager.dict()
+# manager = Manager()
+# u = manager.list()
+# meeting = manager.dict()
+
 
 print(mydb)
 myquery = {"$and": [{"meetingId" : "d379fb14d262b7cd51d768079ad59cf08db23287-1609944939692"}, {"length" : 0}, {"locale.locale" : "en"}]}
@@ -20,36 +24,76 @@ myquery = {"$and": [{"meetingId" : "d379fb14d262b7cd51d768079ad59cf08db23287-160
 for i in mydb.find(myquery):
     print(i)
 
-def handler(message):
-    message = json.loads(message["data"].decode("UTF-8"))
-    print(message["Event"])
-    if message["Event"] == "KALDI_START":
-        ASR = message["ASR-Channel"]
-        InputChannel = message["Input-Channel"]
-        CallerUsername = message["Caller-Username"]
-        pubsub.subscribe(**{ASR : kaldi_text})
-        thread2 = pubsub.run_in_thread(sleep_time=0.001)
-        print(CallerUsername)
-        print(meeting)
+def the_loop():
+    meetings = {}
+    while True:
+            fullmessage = pubsub.get_message()
+            if fullmessage and not isinstance(fullmessage["data"], int):
+                message = json.loads(fullmessage["data"].decode("UTF-8"))
+                # print(message)
+                try:
+                    if "Event" in message.keys():
+                        if message["Event"] == "KALDI_START":
+                            print("Kaldi is started. Lets get ASR!")
+                            ASR = message["ASR-Channel"]
+                            meetingId = message["meetingId"]
+                            OrigCallerIDName = message["Caller-Orig-Caller-ID-Name"]
+                            meetings = dict_handler(meetings, meetingId, ASR, OrigCallerIDName)
+                            print(meetings)
+                            pubsub.subscribe(ASR)
+                            # Loader_Start_msg = {"Event" : "KALDI_START", "Caller-Destination-Number" : CallerDestinationNumber, "meetingId" : meetingId, "Caller-Orig-Caller-ID-Name" : OrigCallerIDName, 'Caller-Username': CallerUsername, "Input-Channel" : input_channel, "ASR-Channel" : output_channel}
+                        
+                    if message["handle"] == "partialUtterance":
+                        print(fullmessage)
+                        print(message)
+                        print(message["utterance"])
+                        # meetings[]
+                        # Loader_Stop_msg = {"Event" : "KALDI_STOP", "Caller-Destination-Number" : CallerDestinationNumber, "meetingId" : meetingId, "Caller-Orig-Caller-ID-Name" : OrigCallerIDName, 'Caller-Username': CallerUsername, "Input-Channel" : input_channel, "ASR-Channel" : output_channel}
+                except:
+                    pass
+
+def dict_handler(d, meetingId, ASR, participant):
+    if ASR not in d.keys():
+        d[ASR] = {}
+        d[ASR]["meetingId"] = meetingId
+        d[ASR]["participants"] = {}
+    d[ASR]["participants"][participant] = ""
+    return d
+
+
+
+if __name__ == "__main__":
+    the_loop()
+
+# def handler(message):
+#     message = json.loads(message["data"].decode("UTF-8"))
+#     print(message["Event"])
+#     if message["Event"] == "KALDI_START":
+#         ASR = message["ASR-Channel"]
+#         InputChannel = message["Input-Channel"]
+#         CallerUsername = message["Caller-Username"]
+#         pubsub.subscribe(**{ASR : kaldi_text})
+#         thread2 = pubsub.run_in_thread(sleep_time=0.001)
+#         print(CallerUsername)
+#         print(meeting)
+#     return message
             
         
+# def kaldi_text(message):
+#     message = json.loads(message["data"].decode("UTF-8"))
+#     if message["handle"] == "partialUtterance":
+#         u.append(message["utterance"])
+#         # print(u)
 
-def kaldi_text(message):
-    message = json.loads(message["data"].decode("UTF-8"))
-    if message["handle"] == "partialUtterance":
-        u.append(message["utterance"])
-        # print(u)
 
-pubsub = red.pubsub()
-pubsub.subscribe(**{"test_channel": handler})
-thread = pubsub.run_in_thread(sleep_time=0.001)
+# message = pubsub.subscribe(**{"test_channel": handler})
+# thread = pubsub.run_in_thread(sleep_time=0.001)
 
-while True:
-    time.sleep(1)
-    ASR = u.pop(0) if u else None
-    if ASR:
-        print(ASR)
-    print(meeting)
+# while True:
+#     time.sleep(1)
+    # ASR = u.pop(0) if u else None
+    # if ASR:
+        # print(ASR)
 
 
 
